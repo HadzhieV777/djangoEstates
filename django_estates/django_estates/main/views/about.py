283d6dedfe58af04115cdda22c475f21f1835daf
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-10 20:46:53 +0300

    re-uploaded

--- django_estates/django_estates/main/views/about.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import mixins as auth_mixins
-from django.views import generic as views
-
-
-class AboutPageView(views.TemplateView):
-    template_name = 'main/about_test.html'
--- django_estates/django_estates/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('django_estates.main.urls')),
-    path('accounts/', include('django_estates.accounts.urls')),
-    path('blog/', include('django_estates.blog.urls')),
-]
