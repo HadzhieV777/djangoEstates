30167ef04d53df4b39a544a75df26fb5b6eb93dd
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-11 21:18:08 +0300

    Edited settigns.py

--- django_estates/django_estates/settings.py
+++ django_estates/django_estates/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)b433)g0*ca+n*m2@)w1dr))q4zmuse8y7-x_=ms=x+5(pnd%0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
