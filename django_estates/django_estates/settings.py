c18571960cbe7afb87eedc99cc6fed7f68c3c037
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-11 20:59:59 +0300

    Edited settings.py

--- django_estates/django_estates/settings.py
+++ django_estates/django_estates/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)b433)g0*ca+n*m2@)w1dr))q4zmuse8y7-x_=ms=x+5(pnd%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+'''
+'False' == 'True' => False
+'True' == 'True'  => True
+'''
 
 ALLOWED_HOSTS = [
     'localhost',
