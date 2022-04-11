4478e673a73463826c8d4fc78ab75177e2a096d8
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-11 21:36:46 +0300

    Edited settings.py

--- django_estates/django_estates/settings.py
+++ django_estates/django_estates/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'sk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -28,11 +28,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 'True' == 'True'  => True
 '''
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'djangoestates.herokuapp.com',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 DJANGO_APPS = (
