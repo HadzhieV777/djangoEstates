b54fb23353e3e1332aae69bb27f3efe32edbdb03
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-13 23:08:52 +0300

    Added subscribe view tests

--- django_estates/tests/newsletter/views/subscribe_view_tests.py
+++ django_estates/tests/newsletter/views/subscribe_view_tests.py
@@ -5,6 +5,10 @@ from django_estates.newsletter.models import Subscribers
 
 
 class SubscribeViewTests(TestCase):
+    VALID_USER_CREDENTIALS = {
+        'username': 'testuser',
+        'password': '1123QwER',
+    }
 
     def test_correct_template__expect_success(self):
         response = self.client.get(reverse('subscribe'))
@@ -16,3 +20,15 @@ class SubscribeViewTests(TestCase):
         self.client.post(reverse('subscribe'), {'email': 'testemail@gmail.com'})
         self.assertIsNotNone(subscriber.pk)
 
+    def test_when_user_adds_email__expect_success(self):
+        self.client.login(**self.VALID_USER_CREDENTIALS)
+
+        response = self.client.post(
+            reverse('subscribe'),
+            data={
+                'email': 'testemail@gmail.com',
+
+            }
+        )
+        subscribers = Subscribers.objects.filter(email='testemail@gmail.com').exists()
+        self.assertTrue(subscribers)
